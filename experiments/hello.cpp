61e73550918739153664520b924a8b590af0250e
Ben Pyrik <ben@wsundine.com>
2018-10-01 13:28:49 -0400

    Add explanation of #include

--- experiments/hello.cpp
+++ experiments/hello.cpp
@@ -1,6 +1,13 @@
 
-// Bring in "cout"
+// Bring in "cout".
+// "#include" literally gets replaced with code in the <iostream> header file
+// by the C++ preprocessor.
+
+// The preprocessed file can be obtained by running "g++ -E hello.cpp -o hello.ii".
+// It's over 18,000 lines long!
+// https://www.toptal.com/c-plus-plus/c-plus-plus-understanding-compilation
 #include <iostream>
+
 // This dumps std functions into the global scope. So you can write 'cin' instead of 'std::cin'.
 using namespace std;
 
