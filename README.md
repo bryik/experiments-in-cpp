569ce16fc56f5480fdc8629f3f36508eeb2969ab
Ben Pyrik <ben@wsundine.com>
2018-09-30 23:32:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Experiments in C++
+
+This repo contains little examples demonstrating certain concepts from C++. They are slightly cleaned up versions of notes I wrote during the uOttawa course CSI 2372.
---
+++ experiments/hello.cpp
@@ -0,0 +1,12 @@
+
+// Bring in "cout"
+#include <iostream>
+// This dumps std functions into the global scope. So you can write 'cin' instead of 'std::cin'.
+using namespace std;
+
+int main() {
+
+	cout << "Hello world!" << endl;
+	return 0;
+
+}
