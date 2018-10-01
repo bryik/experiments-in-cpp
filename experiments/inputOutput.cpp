5717421a731cf2336ded2aa620546f568519405e
Ben Pyrik <ben@wsundine.com>
2018-10-01 13:29:17 -0400

    Add basic input output example with cout and cin

---
+++ experiments/inputOutput.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  
+  // Read user input up to a space.
+  string msg;
+  // right-shift operator converts and assigns inpu from input stream
+  cin >> msg;
+
+  // Echo it back out with the left-shift operator.
+  // "endl" inserts a new line.
+  cout << msg << endl;
+
+  return 0;
+}
